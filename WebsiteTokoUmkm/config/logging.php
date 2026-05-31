5df52f1ecf3312adb8469999c5496f57c387f26f
Laurentius Dandi Andhika S.Kom <naudenfaiz9@gmail.com>
2026-05-31 11:41:51 +0700

    Update logging.php

--- WebsiteTokoUmkm/config/logging.php
+++ WebsiteTokoUmkm/config/logging.php
@@ -7,31 +7,19 @@ use Monolog\Handler\SyslogUdpHandler;
 return [
 
     /*
+
     |--------------------------------------------------------------------------
     | Default Log Channel
     |--------------------------------------------------------------------------
-    |
-    | This option defines the default log channel that gets used when writing
-    | messages to the logs. The name specified in this option should match
-    | one of the channels defined in the "channels" configuration array.
-    |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'stderr'),
 
     /*
+
     |--------------------------------------------------------------------------
     | Log Channels
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure the log channels for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
-    |
-    | Available Drivers: "single", "daily", "slack", "syslog",
-    |                    "errorlog", "monolog",
-    |                    "custom", "stack"
-    |
     */
 
     'channels' => [
