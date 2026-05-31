049f6ae3a56a6682aaf723dd8ec3db1eb9631071
Laurentius Dandi Andhika S.Kom <naudenfaiz9@gmail.com>
2026-05-31 12:35:20 +0700

    Update index.php

--- WebsiteTokoUmkm/public/index.php
+++ WebsiteTokoUmkm/public/index.php
@@ -1,17 +1,24 @@
 <?php
 
+// Paksa session menggunakan cookie agar tidak memicu error Read-Only di Vercel
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_SERVER['SESSION_DRIVER'] = 'cookie';
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
 /*
+
 |--------------------------------------------------------------------------
 | Check If Application Is Under Maintenance
 |--------------------------------------------------------------------------
+
 |
 | If the application is maintenance / demo mode via the "down" command we
 | will require this file so that any prerendered template can be shown
+
 | instead of starting the framework, which could cause an exception.
 |
 */
@@ -21,12 +28,15 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 }
 
 /*
+
 |--------------------------------------------------------------------------
 | Register The Auto Loader
 |--------------------------------------------------------------------------
+
 |
 | Composer provides a convenient, automatically generated class loader for
 | this application. We just need to utilize it! We'll simply require it
+
 | into the script here so we don't need to manually load our classes.
 |
 */
@@ -34,12 +44,15 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 require __DIR__.'/../vendor/autoload.php';
 
 /*
+
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
+
 |
 | Once we have the application, we can handle the incoming request using
 | the application's HTTP kernel. Then, we will send the response back
+
 | to this client's browser, allowing them to enjoy our application.
 |
 */
