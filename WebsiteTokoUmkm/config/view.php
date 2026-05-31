e2d26040e0594cf31a9418222a52c61b68382a56
Laurentius Dandi Andhika S.Kom <naudenfaiz9@gmail.com>
2026-05-31 21:15:31 +0700

    Update view.php

--- WebsiteTokoUmkm/config/view.php
+++ WebsiteTokoUmkm/config/view.php
@@ -3,14 +3,6 @@
 // Cek apakah berjalan di server Vercel
 $isVercel = isset($_ENV['VERCEL_JOB_ID']) || isset($_SERVER['VERCEL_URL']);
 
-// Tentukan folder penyimpanan view cache secara dinamis
-$compiledPath = $isVercel ? '/tmp/storage/framework/views' : realpath(storage_path('framework/views'));
-
-// Buat foldernya secara paksa jika berjalan di Vercel dan belum ada
-if ($isVercel && !is_dir($compiledPath)) {
-    mkdir($compiledPath, 0755, true);
-}
-
 return [
 
     /*
@@ -18,13 +10,6 @@ return [
     |--------------------------------------------------------------------------
     | View Storage Paths
     |--------------------------------------------------------------------------
-
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views. Of course
-
-    | the usual Laravel view path has already been registered for you.
-    |
     */
 
     'paths' => [
@@ -38,13 +23,16 @@ return [
     |--------------------------------------------------------------------------
 
     |
-    | This option determines where all the compiled Blade templates will be
-    | stored for your application. Typically, this is within the storage
+    | 💡 FIX PERMANEN VERCEL: 
+    | Jika di Vercel, arahkan LANGSUNG ke root folder '/tmp'. 
 
-    | directory. However, as usual, you are free to change this value.
+    | Folder '/tmp' dijamin sudah ada dan berstatus Writable (bisa ditulis) 
+    | tanpa perlu repot menjalankan fungsi mkdir() yang rawan diblokir.
     |
     */
 
-    'compiled' => $compiledPath,
+    'compiled' => $isVercel 
+        ? '/tmp' 
+        : env('VIEW_COMPILED_PATH', realpath(storage_path('framework/views'))),
 
 ];
