57f86d987267dfa12d85fe5f0c2751bc67a7211c
Laurentius Dandi Andhika S.Kom <naudenfaiz9@gmail.com>
2026-05-31 11:45:08 +0700

    Update web.php

--- WebsiteTokoUmkm/routes/web.php
+++ WebsiteTokoUmkm/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\ProductController;
 
 // Rute untuk pembeli melihat katalog depan
@@ -11,7 +12,15 @@ Route::get('/admin', [ProductController::class, 'adminDashboard'])->middleware('
 Route::post('/admin/tambah', [ProductController::class, 'store'])->middleware('auth');
 Route::delete('/admin/hapus/{id}', [ProductController::class, 'destroy'])->middleware('auth');
 
-// Tambahan otomatis rute bawaan Laravel Auth (Biarkan saja)
+// Tambahan otomatis rute bawaan Laravel Auth
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route darurat untuk membersihkan cache konfigurasi di Vercel
+Route::get('/clear-vercel', function() {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    return "Semua cache di Vercel sudah bersih! Silakan kembali ke halaman utama.";
+});
