602b43741a5f9e4781ed93f15b17ca28a6ede6e6
Laurentius Dandi Andhika S.Kom <naudenfaiz9@gmail.com>
2026-05-31 11:50:28 +0700

    Create ignition.php

---
+++ WebsiteTokoUmkm/config/ignition.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+
+    |--------------------------------------------------------------------------
+    | Register Error Page Reporting
+    |--------------------------------------------------------------------------
+    | Mematikan penyimpanan log lokal agar tidak memicu error Read-Only di Vercel
+    */
+    'enable_runnable_solutions' => false,
+    'enable_share_button' => false,
+    'top_level_arguments_count' => 4,
+    'local_config_file' => '',
+];
