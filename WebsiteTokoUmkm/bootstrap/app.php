0b23b40127b11e764db319195add527be8b20655
Laurentius Dandi Andhika S.Kom <naudenfaiz9@gmail.com>
2026-05-31 12:49:37 +0700

    Update app.php

--- WebsiteTokoUmkm/bootstrap/app.php
+++ WebsiteTokoUmkm/bootstrap/app.php
@@ -1,12 +1,15 @@
 <?php
 
 /*
+
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
+
 |
 | The first thing we will do is create a new Laravel application instance
 | which serves as the "glue" for all the components of Laravel, and is
+
 | the IoC container for the system binding all of the various parts.
 |
 */
@@ -16,13 +19,16 @@ $app = new Illuminate\Foundation\Application(
 );
 
 /*
+
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
 |--------------------------------------------------------------------------
+
 |
 | Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
+| that we will be able to resolve them when needed. The kernels serve
+
+| the incoming requests to this application from both the web and CLI.
 |
 */
 
@@ -42,12 +48,31 @@ $app->singleton(
 );
 
 /*
+
+|--------------------------------------------------------------------------
+| Vercel Custom Patches (Trik Bypass Mutlak Read-Only)
+|--------------------------------------------------------------------------
+*/
+if (isset($_ENV['VERCEL_JOB_ID']) || isset($_SERVER['VERCEL_URL'])) {
+    // 1. Alihkan semua folder storage log dan view ke /tmp yang bersih
+    $app->useStoragePath('/tmp/storage');
+
+    // 2. PAKSA sistem konfigurasi session Laravel ke cookie agar tidak menulis file fisik
+    $app->booting(function () {
+        config(['session.driver' => 'cookie']);
+    });
+}
+
+/*
+
 |--------------------------------------------------------------------------
 | Return The Application
 |--------------------------------------------------------------------------
+
 |
 | This script returns the application instance. The instance is given to
 | the calling script so we can separate the building of the instances
+
 | from the actual running of the application and sending responses.
 |
 */
