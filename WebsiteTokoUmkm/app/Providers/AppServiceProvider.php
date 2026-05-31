9f5e69aca636a0b7ba3e33220b19cb648b91209c
Laurentius Dandi Andhika S.Kom <naudenfaiz9@gmail.com>
2026-05-31 11:37:27 +0700

    Update AppServiceProvider.php

--- WebsiteTokoUmkm/app/Providers/AppServiceProvider.php
+++ WebsiteTokoUmkm/app/Providers/AppServiceProvider.php
@@ -11,10 +11,18 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        //
+   public function register()
+{
+    // Jika berjalan di server Vercel, pindahkan folder storage ke /tmp
+    if (isset($_ENV['VERCEL_JOB_ID']) || isset($_SERVER['VERCEL_URL'])) {
+        $this->app->useStoragePath('/tmp/storage');
+        
+        // Buat folder yang dibutuhkan jika belum ada
+        if (!is_dir('/tmp/storage/framework/views')) {
+            mkdir('/tmp/storage/framework/views', 0755, true);
+        }
     }
+}
 
     /**
      * Bootstrap any application services.
